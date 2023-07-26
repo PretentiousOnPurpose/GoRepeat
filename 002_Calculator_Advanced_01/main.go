a6a988f6a980e25e477b4568799799ba5bd80280
Yashwanth R <yash.rk@outlook.com>
2023-07-26 14:10:06 +0200

    Updates for linkedLists added

--- 002_Calculator_Advanced_01/main.go
+++ 002_Calculator_Advanced_01/main.go
@@ -10,6 +10,49 @@ import (
 
 // Capture calculation history via LinkedLists
 
+type Calculation struct {
+	cmd string
+	res float64
+}
+
+type CalcHistory struct {
+	calc     Calculation
+	nextNode *CalcHistory
+	prevNode *CalcHistory
+}
+
+func (calc *Calculation) printCalculation() {
+	fmt.Printf("%s = %f\n", calc.cmd, calc.res)
+}
+
+func (root *CalcHistory) printFwdHistory() {
+	currNode := root
+
+	for i := 0; ; i++ {
+		fmt.Printf("Calc %d: ", i)
+		currNode.calc.printCalculation()
+		currNode = currNode.nextNode
+
+		if currNode == nil {
+			break
+		}
+	}
+}
+
+func (lastNode *CalcHistory) printBwdHistory() {
+	currNode := lastNode
+
+	for i := 0; ; i++ {
+		fmt.Printf("Calc %d: ", i)
+		currNode.calc.printCalculation()
+		currNode = currNode.prevNode
+
+		if currNode == nil {
+			break
+		}
+	}
+}
+
 func main() {
 	var sym string
 	var operands []string
@@ -26,38 +69,42 @@ func main() {
 			fmt.Println("Parsing: input cmd failed")
 		}
 
-		// Capture additional input formats: without spaces.
-
 		operands = strings.Split(cmd, " ")
 
-		n1, err := strconv.ParseFloat(strings.TrimSpace(operands[0]), 64)
-		if err != nil {
-			fmt.Println("Parsing: N1 failed")
-		}
-
-		sym = strings.TrimSpace(operands[1])
-
-		n2, err := strconv.ParseFloat(strings.TrimSpace(operands[2]), 64)
-		if err != nil {
-			fmt.Println("Parsing: N2 failed")
-		}
-
-		switch sym {
-		case "+":
-			res = n1 + n2
-		case "-":
-			res = n1 - n2
-		case "*":
-			res = n1 * n2
-		case "/":
-			if n2 != 0 {
-				res = n1 / n2
-			} else {
-				fmt.Printf("Result: Division by zero error\n")
-				break
+		if len(operands) == 3 {
+			n1, err := strconv.ParseFloat(strings.TrimSpace(operands[0]), 64)
+			if err != nil {
+				fmt.Println("Parsing: N1 failed")
 			}
-		}
 
-		fmt.Printf("Result: %v\n", res)
+			sym = strings.TrimSpace(operands[1])
+
+			n2, err := strconv.ParseFloat(strings.TrimSpace(operands[2]), 64)
+			if err != nil {
+				fmt.Println("Parsing: N2 failed")
+			}
+
+			switch sym {
+			case "+":
+				res = n1 + n2
+			case "-":
+				res = n1 - n2
+			case "*":
+				res = n1 * n2
+			case "/":
+				if n2 != 0 {
+					res = n1 / n2
+				} else {
+					fmt.Printf("Result: Division by zero error\n")
+					break
+				}
+			}
+
+			// Add this calc to the history
+
+			fmt.Printf("Result: %v\n", res)
+		} else {
+			// Process other commands
+		}
 	}
 }
