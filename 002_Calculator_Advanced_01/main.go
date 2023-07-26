9f38d697787b0c113bf49d0bd76b42d32c7d112d
Yashwanth R <yash.rk@outlook.com>
2023-07-26 20:44:42 +0200

    Calc Adv 1.0 is working and complete

--- 002_Calculator_Advanced_01/main.go
+++ 002_Calculator_Advanced_01/main.go
@@ -23,11 +23,12 @@ type CalcHistory struct {
 }
 
 func (calc *Calculation) printCalculation() {
-	fmt.Printf("%s = %f\n", calc.cmd, calc.res)
+	fmt.Printf("%s = %v\n", calc.cmd, calc.res)
 }
 
 func (root *CalcHistory) printFwdHistory() {
 	currNode := root
+	fmt.Println("---------------------------------")
 
 	for i := 0; ; i++ {
 		fmt.Printf("Calc %d: ", i)
@@ -38,10 +39,12 @@ func (root *CalcHistory) printFwdHistory() {
 			break
 		}
 	}
+	fmt.Println("---------------------------------")
 }
 
 func (lastNode *CalcHistory) printBwdHistory() {
 	currNode := lastNode
+	fmt.Println("---------------------------------")
 
 	for i := 0; ; i++ {
 		fmt.Printf("Calc %d: ", i)
@@ -52,6 +55,7 @@ func (lastNode *CalcHistory) printBwdHistory() {
 			break
 		}
 	}
+	fmt.Println("---------------------------------")
 }
 
 func main() {
@@ -63,13 +67,9 @@ func main() {
 	currNode := root
 
 	fmt.Println("Calculator: An Advanced Take v1.0")
-	fmt.Println("------------------------")
+	fmt.Println("---------------------------------")
 
 	for {
-		if currNode.calc != nil {
-			currNode = currNode.nextNode
-		}
-
 		fmt.Printf("Input cmd: ")
 		stdIn := bufio.NewReader(os.Stdin)
 		cmd, err := stdIn.ReadString('\n')
@@ -80,6 +80,13 @@ func main() {
 		operands = strings.Split(cmd, " ")
 
 		if len(operands) == 3 {
+
+			if currNode.calc != nil {
+				currNode.nextNode = &CalcHistory{}
+				currNode.nextNode.prevNode = currNode
+				currNode = currNode.nextNode
+			}
+
 			n1, err := strconv.ParseFloat(strings.TrimSpace(operands[0]), 64)
 			if err != nil {
 				fmt.Println("Parsing: N1 failed")
@@ -110,9 +117,7 @@ func main() {
 			}
 
 			// Add this calc to the history
-			currNode.calc = &Calculation{cmd, res}
-			currNode.nextNode = &CalcHistory{}
-			currNode.nextNode.prevNode = currNode
+			currNode.calc = &Calculation{strings.TrimSpace(cmd), res}
 
 			fmt.Printf("Result: %v\n", res)
 		} else if len(operands) == 2 {
@@ -120,11 +125,20 @@ func main() {
 			if strings.ToLower(strings.TrimSpace(operands[1])) == "history" {
 				if strings.ToLower(strings.TrimSpace(operands[0])) == "forward" {
 					root.printFwdHistory()
-				} else if strings.ToLower(strings.TrimSpace(operands[1])) == "backward" {
-					currNode.printFwdHistory()
+				} else if strings.ToLower(strings.TrimSpace(operands[0])) == "backward" {
+					currNode.printBwdHistory()
 				}
 			}
 
+		} else if len(operands) == 1 {
+			if strings.ToLower(strings.TrimSpace(operands[0])) == "exit" {
+				fmt.Println("---------------------------------")
+				fmt.Println("Calculator is shutting down...")
+				fmt.Println("Ciao!")
+				os.Exit(0)
+			} else {
+				fmt.Printf("Error: unknown cmd %s", strings.TrimSpace(operands[0]))
+			}
 		}
 	}
 }
